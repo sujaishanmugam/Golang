1454517016ef66bff2d28ccd57ef5ae864af9e24
sujaishanmugam <sujaishanmugam26@gmail.com>
2021-06-03 22:42:32 +0530

    switch

---
+++ switch.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("enter the number")
+	var a int
+	fmt.Scanln(&a)
+	switch a {
+	case 10:
+		fmt.Println(" a is 10")
+	case 20:
+		fmt.Println("a is 20")
+
+	default:
+		fmt.Println("No Luck")
+	}
+
+}
