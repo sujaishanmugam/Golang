39c5e63d49dc8c6f14936a65a5c78bc01913d324
sujaishanmugam <sujaishanmugam26@gmail.com>
2021-06-03 22:26:24 +0530

    desision

--- array.go
+++ array.go
@@ -1,29 +1,29 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-func main() {
-	// 1D array
-	// var a = []int{0, 1, 2, 3, 4, 6}
-	// var b = a[0:]
-	// fmt.Println(b)
+// func main() {
+// 	// 1D array
+// 	var a = []int{0, 1, 2, 3, 4, 6}
+// 	var b = a[0:]
+// 	fmt.Println(b)
 
-	// 2D array
-	// var a = []int{0, 1, 2, 3, 4, 6,}
-	// var b = [][]int{
-	// 	{1, 2},
-	// 	{3, 4},
-	// 	{5, 6},
-	// }
-	// fmt.Println(b)
+// 	// 2D array
+// 	var a = []int{0, 1, 2, 3, 4, 6,}
+// 	var b = [][]int{
+// 		{1, 2},
+// 		{3, 4},
+// 		{5, 6},
+// 	}
+// 	fmt.Println(b)
 
-	// struct in arrays
-	b := []struct {
-		name string
-		i    bool
-	}{
-		{"sujai", true},
-		{"vijay", false},
-	}
-	fmt.Println(b)
-}
+// 	// struct in arrays
+// 	b := []struct {
+// 		name string
+// 		i    bool
+// 	}{
+// 		{"sujai", true},
+// 		{"vijay", false},
+// // 	}
+// 	fmt.Println(b)
+// }
---
+++ desision.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 10
+	var b int = 9
+	if a == b {
+		fmt.Println("a is equal to b")
+	} else {
+		fmt.Println("a is not equal to b")
+	}
+	if a > b {
+		fmt.Println(" a is greater than b ")
+	}
+	if a != b {
+		fmt.Println(" a is not equal to b")
+	}
+}
